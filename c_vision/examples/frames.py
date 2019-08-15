a5d923ea241fd7165f29b51c98a6ed0239881947
mugoh <mugoh.ks@gmail.com>
2019-08-15 08:46:48 +0300

    save capture

--- c_vision/examples/frames.py
+++ c_vision/examples/frames.py
@@ -18,7 +18,7 @@ def get_frame():
             cv2.imshow('Input frame', frame)
             gray_scaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             cv2.imshow('gray scaled', gray_scaled)
-
+            save(frame, gray_scaled)
             if cv2.waitKey(20) & 0xff == ord('q'):
                 break
         else:
@@ -26,6 +26,18 @@ def get_frame():
 
     capture.release()
     cv2.destroyAllWindows()
+
+
+def save(frame, gray_frame):
+    """
+        Saves captued frames
+    """
+    idx = 0
+    if cv2.waitKey(20) & 0xff == ord('s'):
+        name = f'cature_{idx}.png'
+        gray_frame_n = 'grayscale_capture_{idx}.png'
+        cv2.imwrite(name, frame)
+        cv2.imwrite(gray_frame_n, gray_frame)
 
 
 if __name__ == '__main__':
